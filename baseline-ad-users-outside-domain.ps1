a5f7c2c4cd8fa78bcbe9e91dcd08170e484de2c5
petescode <mrpmwood@gmail.com>
2019-09-28 09:31:11 -0700

    initial working script

---
+++ baseline-ad-users-outside-domain.ps1
@@ -0,0 +1,20 @@
+Clear-Host
+Import-Module ActiveDirectory
+[string]$datestamp = (Get-Date).ToString('yyyy-MM-dd')
+[string]$timestamp = (Get-Date).ToString('HHmm')
+[string]$report_name = "ad_users_baseline_$datestamp@$($timestamp).txt"
+
+# directory to store reports in; if not exist, create
+$path_test = Test-Path "$env:USERPROFILE\Desktop\$((Get-Date).ToString('yyyy-MM-dd'))_powershell_queries"
+if($path_test -ne $TRUE){ $directory = New-Item -ItemType Directory -Path "$env:USERPROFILE\Desktop\$((Get-Date).ToString('yyyy-MM-dd'))_powershell_queries" }
+else{ $directory = "$env:USERPROFILE\Desktop\$((Get-Date).ToString('yyyy-MM-dd'))_powershell_queries" }
+
+[string]$dc = Read-Host "Enter domain controller IP"
+[string]$user = Read-Host "Enter domain username"
+
+[array]$report = Get-ADUser -Filter * -Server $dc -Credential $user `
+    -Properties Name,samAccountName,Enabled,Created,Modified,lastLogonDate,PasswordLastSet `
+    | Select Name,samAccountName,Enabled,Created,Modified,lastLogonDate,PasswordLastSet `
+    | Sort samAccountName
+
+$report | Tee-Object -FilePath "$directory\$report_name"
