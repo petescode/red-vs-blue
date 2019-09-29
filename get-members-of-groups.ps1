5b8e758d95330dbec7ad16d5112ef95d08b882ee
petescode <mrpmwood@gmail.com>
2019-09-29 10:13:17 -0700

    added output directories

--- get-members-of-groups.ps1
+++ get-members-of-groups.ps1
@@ -3,6 +3,20 @@
 Import-Module ActiveDirectory
 Clear-Host
 [int]$count=0
+[string]$datestamp = (Get-Date).ToString('yyyy-MM-dd')
+[string]$timestamp = (Get-Date).ToString('HHmm')
+[string]$report_name = "ad_groups_with_members_$datestamp@$($timestamp).txt"
+
+# directory to store reports in; if not exist, create
+$path_test = Test-Path "$env:USERPROFILE\Desktop\$((Get-Date).ToString('yyyy-MM-dd'))_powershell_queries"
+if($path_test -ne $TRUE){ $directory = New-Item -ItemType Directory -Path "$env:USERPROFILE\Desktop\$((Get-Date).ToString('yyyy-MM-dd'))_powershell_queries" }
+else{ $directory = "$env:USERPROFILE\Desktop\$((Get-Date).ToString('yyyy-MM-dd'))_powershell_queries" }
+
+# sub-directory for organization
+$path_test = Test-Path "$directory\ad_group_baselines"
+if($path_test -ne $TRUE){ $subdir = New-Item -ItemType Directory -Path "$directory\ad_group_baselines" }
+else{ $subdir = "$directory\ad_group_baselines" }
+
 
 [ipaddress]$dc = Read-Host "Enter domain controller IP"
 [string]$user = Read-Host "Enter domain username"
@@ -19,13 +33,8 @@ Get-ADGroup -Filter * -Server $dc -Credential $cred -Properties Members | ForEac
     $object | Add-Member -Type NoteProperty -Name "Members" `
         -Value $(Get-ADGroupMember -Server $dc -Credential $cred -Identity $_.Name | Select -Expand Name)
     [array]$array += $object
-    
-    #Get-ADGroupMember -Server $dc -Credential $cred -Identity $_.Name | ForEach{
-        #Write-Host $_.Name
-        #$object | Add-Member -Type NoteProperty -Name "Members" -Value $_.Name
-        #[array]$array += $object
-    #}
 }
-$array | Out-Host
+#$array | Select Name,Members | Select -Expand Members Out-Host
+$array | Format-List | Tee-Object -FilePath "$subdir\$report_name" 
 
 # report on the count at some point
