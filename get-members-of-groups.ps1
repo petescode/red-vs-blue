87442d9748fbca709849ce37c52b94e27ada0162
petescode <mrpmwood@gmail.com>
2019-09-29 09:45:19 -0700

    loop to gather group names & members

--- get-members-of-groups.ps1
+++ get-members-of-groups.ps1
@@ -1,7 +1,8 @@
 # Author: Pete Wood
 # developed in PowerShell 5.1
-Clear-Host
 Import-Module ActiveDirectory
+Clear-Host
+[int]$count=0
 
 [ipaddress]$dc = Read-Host "Enter domain controller IP"
 [string]$user = Read-Host "Enter domain username"
@@ -10,4 +11,21 @@ $password = Read-Host "Enter username password" -AsSecureString
 # avoiding interactive pop-up; keep all input at shell for non-GUI OS
 $cred = New-Object System.Management.Automation.PSCredential($user,$password)
 
-Get-ADGroup -Filter * -Server $dc -Credential $cred
+Get-ADGroup -Filter * -Server $dc -Credential $cred -Properties Members | ForEach-Object {
+    
+    $count++
+    $object = New-Object System.Object
+    $object | Add-Member -Type NoteProperty -Name "Name" -Value $_.Name
+    $object | Add-Member -Type NoteProperty -Name "Members" `
+        -Value $(Get-ADGroupMember -Server $dc -Credential $cred -Identity $_.Name | Select -Expand Name)
+    [array]$array += $object
+    
+    #Get-ADGroupMember -Server $dc -Credential $cred -Identity $_.Name | ForEach{
+        #Write-Host $_.Name
+        #$object | Add-Member -Type NoteProperty -Name "Members" -Value $_.Name
+        #[array]$array += $object
+    #}
+}
+$array | Out-Host
+
+# report on the count at some point
