014d966b1c8b2ea6e63d9b4e508485c33f62f88b
petescode <mrpmwood@gmail.com>
2019-09-30 09:25:17 -0700

    initial working script in baseline-ad-objects

---
+++ baseline-ad-objects-outside-domain.ps1
@@ -0,0 +1,54 @@
+# Author: Pete Wood
+# developed in PowerShell 5.1
+
+# check that all objects are sorted properly before being output to ensure consistency
+# update credential method
+
+Import-Module ActiveDirectory
+Clear-Host
+[string]$datestamp = (Get-Date).ToString('yyyy-MM-dd')
+[string]$timestamp = (Get-Date).ToString('HHmm')
+[string]$computers_file = "ad_computers_baseline_$datestamp@$($timestamp).txt"
+[string]$users_file = "ad_users_baseline_$datestamp@$($timestamp).txt"
+[string]$groups_file = "ad_groups_baseline_$datestamp@$($timestamp).txt"
+
+# directory to store reports in; if not exist, create
+$path_test = Test-Path "$env:USERPROFILE\Desktop\$((Get-Date).ToString('yyyy-MM-dd'))_powershell_queries"
+if($path_test -ne $TRUE){ $directory = New-Item -ItemType Directory -Path "$env:USERPROFILE\Desktop\$((Get-Date).ToString('yyyy-MM-dd'))_powershell_queries" }
+else{ $directory = "$env:USERPROFILE\Desktop\$((Get-Date).ToString('yyyy-MM-dd'))_powershell_queries" }
+
+# sub-directory for computers
+$path_test = Test-Path "$directory\ad_computer_baselines"
+if($path_test -ne $TRUE){ $subdir_comps = New-Item -ItemType Directory -Path "$directory\ad_computer_baselines" }
+else{ $subdir_comps = "$directory\ad_computer_baselines" }
+
+# sub-directory for users
+$path_test = Test-Path "$directory\ad_user_baselines"
+if($path_test -ne $TRUE){ $subdir_users = New-Item -ItemType Directory -Path "$directory\ad_user_baselines" }
+else{ $subdir_users = "$directory\ad_user_baselines" }
+
+# sub-directory for groups
+$path_test = Test-Path "$directory\ad_group_baselines"
+if($path_test -ne $TRUE){ $subdir_groups = New-Item -ItemType Directory -Path "$directory\ad_group_baselines" }
+else{ $subdir_groups = "$directory\ad_group_baselines" }
+
+[ipaddress]$dc = Read-Host "Enter domain controller IP"
+[string]$user = Read-Host "Enter domain username"
+
+[array]$computers = Get-ADComputer -Filter * -Server $dc -Credential $user `
+    -Properties Name,samAccountName,Enabled,Created,Modified,lastLogonDate,OperatingSystem,OperatingSystemVersion,PasswordNotRequired `
+    | Select Name,samAccountName,Enabled,Created,Modified,lastLogonDate,OperatingSystem,OperatingSystemVersion,PasswordNotRequired `
+    | Sort Name
+$computers | Out-File -FilePath "$subdir_comps\$computers_file"
+
+[array]$users = Get-ADUser -Filter * -Server $dc -Credential $user `
+    -Properties Name,samAccountName,Enabled,Created,Modified,lastLogonDate,PasswordLastSet `
+    | Select Name,samAccountName,Enabled,Created,Modified,lastLogonDate,PasswordLastSet `
+    | Sort samAccountName
+$users | Out-File -FilePath "$subdir_users\$users_file"
+
+[array]$groups = Get-ADGroup -Filter * -Server $dc -Credential $user `
+    -Properties Name,samAccountName,objectClass,Created,Modified,Description,Members `
+    | Select Name,samAccountName,objectClass,Created,Modified,Description,Members `
+    | Sort Name
+$groups | Out-File -FilePath "$subdir_groups\$groups_file"
