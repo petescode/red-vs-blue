9492cac921a079100cd5c0836a6014d3df681d2f
petescode <mrpmwood@gmail.com>
2019-09-30 09:37:53 -0700

    updated credential capture

--- baseline-ad-objects-outside-domain.ps1
+++ baseline-ad-objects-outside-domain.ps1
@@ -1,8 +1,14 @@
-# Author: Pete Wood
-# developed in PowerShell 5.1
+<#
+Author: Pete Wood
 
-# check that all objects are sorted properly before being output to ensure consistency
-# update credential method
+- Developed in PowerShell 5.1
+
+Dev:
+- check that all objects are sorted properly before being output to ensure consistency
+- update credential method
+#>
+########################################################################################
+# Housekeeping
 
 Import-Module ActiveDirectory
 Clear-Host
@@ -31,23 +37,26 @@ else{ $subdir_users = "$directory\ad_user_baselines" }
 $path_test = Test-Path "$directory\ad_group_baselines"
 if($path_test -ne $TRUE){ $subdir_groups = New-Item -ItemType Directory -Path "$directory\ad_group_baselines" }
 else{ $subdir_groups = "$directory\ad_group_baselines" }
+########################################################################################
 
 [ipaddress]$dc = Read-Host "Enter domain controller IP"
 [string]$user = Read-Host "Enter domain username"
+$password = Read-Host "Enter domain username password" -AsSecureString
+$cred = New-Object System.Management.Automation.PSCredential($user,$password)
 
-[array]$computers = Get-ADComputer -Filter * -Server $dc -Credential $user `
+[array]$computers = Get-ADComputer -Filter * -Server $dc -Credential $cred `
     -Properties Name,samAccountName,Enabled,Created,Modified,lastLogonDate,OperatingSystem,OperatingSystemVersion,PasswordNotRequired `
     | Select Name,samAccountName,Enabled,Created,Modified,lastLogonDate,OperatingSystem,OperatingSystemVersion,PasswordNotRequired `
     | Sort Name
 $computers | Out-File -FilePath "$subdir_comps\$computers_file"
 
-[array]$users = Get-ADUser -Filter * -Server $dc -Credential $user `
+[array]$users = Get-ADUser -Filter * -Server $dc -Credential $cred `
     -Properties Name,samAccountName,Enabled,Created,Modified,lastLogonDate,PasswordLastSet `
     | Select Name,samAccountName,Enabled,Created,Modified,lastLogonDate,PasswordLastSet `
     | Sort samAccountName
 $users | Out-File -FilePath "$subdir_users\$users_file"
 
-[array]$groups = Get-ADGroup -Filter * -Server $dc -Credential $user `
+[array]$groups = Get-ADGroup -Filter * -Server $dc -Credential $cred `
     -Properties Name,samAccountName,objectClass,Created,Modified,Description,Members `
     | Select Name,samAccountName,objectClass,Created,Modified,Description,Members `
     | Sort Name
